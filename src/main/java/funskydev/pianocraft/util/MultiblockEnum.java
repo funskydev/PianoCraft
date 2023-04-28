01c0baec3fef6a2fcd26b502776be5900602ed8c
FunskyDev <funskydev@outlook.com>
2023-04-28 21:53:05 +0200

    LinkedHashMap for blocks in MultiblockEnum
    fixes the order for the blocks registry

--- src/main/java/funskydev/pianocraft/util/MultiblockEnum.java
+++ src/main/java/funskydev/pianocraft/util/MultiblockEnum.java
@@ -6,36 +6,33 @@ import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public enum MultiblockEnum {
 
-    PIANO(Map.of(
-            BlockPosEnum.EAST, VoxelShapes.combineAndSimplify(Block.createCuboidShape(0, 0, 1, 8, 15, 16), Block.createCuboidShape(0, 15, 9, 8, 16, 16), BooleanBiFunction.OR),
-            BlockPosEnum.WEST, VoxelShapes.combineAndSimplify(Block.createCuboidShape(8, 0, 1, 16, 15, 16), Block.createCuboidShape(8, 15, 9, 16, 16, 16), BooleanBiFunction.OR),
-            BlockPosEnum.TOP, Block.createCuboidShape(0, 0, 9, 16, 9, 16),
-            BlockPosEnum.TOP_EAST, Block.createCuboidShape(0, 0, 9, 8, 9, 16),
-            BlockPosEnum.TOP_WEST, Block.createCuboidShape(8, 0, 9, 16, 9, 16)
-    ), VoxelShapes.combineAndSimplify(Block.createCuboidShape(0, 0, 1, 16, 15, 16), Block.createCuboidShape(0, 15, 9, 16, 16, 16), BooleanBiFunction.OR));
+    PIANO(new LinkedHashMap<>() {{
+            put(BlockPosEnum.EAST,VoxelShapes.combineAndSimplify(Block.createCuboidShape(0,0,1,8,15,16),Block.createCuboidShape(0,15,9,8,16,16),BooleanBiFunction.OR));
+            put(BlockPosEnum.WEST,VoxelShapes.combineAndSimplify(Block.createCuboidShape(8,0,1,16,15,16),Block.createCuboidShape(8,15,9,16,16,16),BooleanBiFunction.OR));
+            put(BlockPosEnum.TOP,Block.createCuboidShape(0,0,9,16,9,16));
+            put(BlockPosEnum.TOP_EAST,Block.createCuboidShape(0,0,9,8,9,16));
+            put(BlockPosEnum.TOP_WEST,Block.createCuboidShape(8,0,9,16,9,16));
+    }}, VoxelShapes.combineAndSimplify(Block.createCuboidShape(0, 0, 1, 16, 15, 16), Block.createCuboidShape(0, 15, 9, 16, 16, 16), BooleanBiFunction.OR));
 
     private Map<BlockPosEnum, VoxelShape> blocks;
     private MultiblockMainPart mainBlock;
     private VoxelShape mainBlockShape = VoxelShapes.fullCube();
 
-    MultiblockEnum(Map<BlockPosEnum, VoxelShape> blocks, VoxelShape mainBlockShape) {
+    MultiblockEnum(LinkedHashMap<BlockPosEnum, VoxelShape> blocks, VoxelShape mainBlockShape) {
         this.blocks = blocks;
         this.mainBlockShape = mainBlockShape;
     }
 
-    MultiblockEnum(Map<BlockPosEnum, VoxelShape> blocks) {
+    MultiblockEnum(LinkedHashMap<BlockPosEnum, VoxelShape> blocks) {
         this.blocks = blocks;
     }
 
     MultiblockEnum(List<BlockPosEnum> blocks) {
-        this.blocks = new HashMap<>();
+        this.blocks = new LinkedHashMap<>();
         for (BlockPosEnum pos : blocks) this.blocks.put(pos, VoxelShapes.fullCube());
     }
 
