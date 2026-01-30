131cd59ac6ba5adcbf40b9c9a8e788b4ce8db283
FunskyDev <funskydev@outlook.com>
2026-01-30 22:53:34 +0100

    fix datagen

--- src/main/java/funskydev/pianocraft/datagen/PCModelGen.java
+++ src/main/java/funskydev/pianocraft/datagen/PCModelGen.java
@@ -1,21 +1,12 @@
 package funskydev.pianocraft.datagen;
 
-import funskydev.pianocraft.PCMain;
 import funskydev.pianocraft.registry.PCBlocks;
 import net.fabricmc.fabric.api.client.datagen.v1.provider.FabricModelProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.minecraft.client.data.models.BlockModelGenerators;
 import net.minecraft.client.data.models.ItemModelGenerators;
-import net.minecraft.client.data.models.MultiVariant;
-import net.minecraft.client.data.models.blockstates.MultiVariantGenerator;
-import net.minecraft.client.data.models.model.TexturedModel;
-import net.minecraft.client.model.Model;
-import net.minecraft.resources.Identifier;
-import java.util.Optional;
 
 public class PCModelGen extends FabricModelProvider {
-
-    private static final Identifier PIANO_MODEL_ID = Identifier.fromNamespaceAndPath(PCMain.MOD_ID, "block/piano");
 
     public PCModelGen(FabricDataOutput output) {
         super(output);
@@ -24,14 +15,11 @@ public class PCModelGen extends FabricModelProvider {
     @Override
     public void generateBlockStateModels(BlockModelGenerators blockModelGenerators) {
 
-        /*blockModelGenerators.blockStateOutput.accept(
-                MultiVariantGenerator.dispatch(
-                        PCBlocks.PIANO,
-                                BlockModelGenerators.plainVariant(PIANO_MODEL_ID)
-
-                ).with(BlockModelGenerators.ROTATION_HORIZONTAL_FACING));*/
-
         blockModelGenerators.createNonTemplateHorizontalBlock(PCBlocks.PIANO);
+
+        // add empty multiblock models
+
+        //blockModelGenerators.createAirLikeBlock();
 
     }
 
