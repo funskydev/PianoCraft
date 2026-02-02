14a8a8aa62bb1169eee8cdd988f85aed9f758ba9
FunskyDev <83423806+funskydev@users.noreply.github.com>
2026-02-03 00:34:37 +0100

    move data gen to its own source set

--- src/datagen/java/funskydev/pianocraft/datagen/PCDataGen.java
+++ src/datagen/java/funskydev/pianocraft/datagen/PCDataGen.java
@@ -1,7 +1,9 @@
 package funskydev.pianocraft.datagen;
 
+import funskydev.pianocraft.PCMain;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import org.jspecify.annotations.Nullable;
 
 public class PCDataGen implements DataGeneratorEntrypoint {
 
@@ -16,6 +18,12 @@ public class PCDataGen implements DataGeneratorEntrypoint {
         pack.addProvider(PCLootGen::new);
         pack.addProvider(PCRecipeGen::new);
 
+    }
+
+    @Override
+    @Nullable
+    public String getEffectiveModId() {
+        return PCMain.MOD_ID;
     }
 
 }
